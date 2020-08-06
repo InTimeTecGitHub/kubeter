1eb9d8a6da17ad18c59013020daf278f456587e2
Abhai Sasidharan <abhaisasidharan@gmail.com>
2020-08-07 00:17:47 +0530

    Fixed css

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,19 +1,6 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
-rules.push({
-  test: /\.scss$/,
-  use: [{loader: 'style-loader'}, { loader: 'css-loader' }, { loader: 'sass-loader' }],
-});
-rules.push({
-  test: /\.css$/,
-  use: [{loader: 'style-loader'}, { loader: 'css-loader' }],
-});
-rules.push({
-  test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-  use: "url-loader?limit=100000"
-});
-
 module.exports = {
   module: {
     rules,
